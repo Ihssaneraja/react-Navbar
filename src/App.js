7460c04aa044de306fda34ddfd260c2db5f1ae59
Ihssaneraja <contactihssane@gamil.comgit config --global Ihssaneraja ihssaneraja>
2023-10-08 22:31:05 +0200

    first app react check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Container, Navbar, Nav, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#about">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1 className='my-5'>Welcome to My React App</h1>
+        <Card className='mb-3'>
+          <Card.Body >
+            <Card.Title>Card 1</Card.Title>
+            <Card.Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis commodi tempore sapiente quisquam distinctio ipsum numquam tenetur reprehenderit officiis magni nesciunt suscipit cum quod, quidem rem minima, quam a obcaecati. </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className='mb-3'>
+          <Card.Body>
+            <Card.Title>Card 2</Card.Title>
+            <Card.Text> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas consequatur libero suscipit illo corporis architecto omnis, earum assumenda vitae nesciunt quasi animi cumque incidunt vel asperiores perferendis! Saepe, voluptas vero? </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className='mb-3'>
+          <Card.Body>
+            <Card.Title>Card 3</Card.Title>
+            <Card.Text> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ratione repellat esse impedit soluta ad error repellendus, repudiandae unde eius molestiae autem perferendis ab sapiente fugiat velit, commodi deleniti amet.</Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
+    
+    
+    
+    
+    
+    
+    </>
   );
 }
 
